5a5c07832b6d2379a31a812e5c61dcbbf64e39c6
Vladimir Haiduk <nightshade2903@gmail.com>
2022-09-06 22:37:40 +0300

    update

--- src/main/java/com/haiduk/springcourse/controllers/BooksController.java
+++ src/main/java/com/haiduk/springcourse/controllers/BooksController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +38,7 @@ public class BooksController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("book") Book book, BindingResult bindingResult){
+    public String create(@ModelAttribute("book") @Validated Book book, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             return "books/new";
         }
