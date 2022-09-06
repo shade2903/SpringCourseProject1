f599df29f3e5696e81b8f76d3cf92697cca5294b
shade2903 <nighshade2903@gmail.com>
2022-09-06 16:53:23 +0300

    add book controller+

--- src/main/java/com/haiduk/springcourse/controllers/BooksController.java
+++ src/main/java/com/haiduk/springcourse/controllers/BooksController.java
@@ -2,20 +2,24 @@ package com.haiduk.springcourse.controllers;
 
 import com.haiduk.springcourse.dao.BookDAO;
 import com.haiduk.springcourse.dao.PersonDAO;
+import com.haiduk.springcourse.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/books")
-public class BookController {
+public class BooksController {
     private final BookDAO bookDAO;
     private final PersonDAO personDAO;
 
     @Autowired
-    public BookController(BookDAO bookDAO, PersonDAO personDAO) {
+    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
         this.bookDAO = bookDAO;
         this.personDAO = personDAO;
     }
@@ -24,6 +28,20 @@ public class BookController {
     public String index(Model model) {
         model.addAttribute("books", bookDAO.index());
         return "books/index";
+    }
 
+        @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book){
+
+        return "books/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("book") Book book, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "books/new";
+        }
+        bookDAO.save(book);
+        return "redirect:/books";
     }
 }
